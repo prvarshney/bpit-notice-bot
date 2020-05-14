f9ab93bc8ff0712651a89ac6c76abcb21d79eac6
Prashant Varshney <pv03158@gmail.com>
2020-05-14 18:14:09 +0530

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const scrapper = require('./lib/scrapper');
 const downloadNoticeFiles = require('./lib/noticeBoard');
+const broadcaster = require('./lib/broadcaster');
 const fs = require('fs');
 const readline = require('readline');
+const credentials = require('./credentials.json');
+const sleep = require('sleep');
 
 const reader = readline.createInterface({
     input: process.stdin,
@@ -11,7 +14,10 @@ const reader = readline.createInterface({
 async function index() {
     try {
         await scrapper.scrapeNoticeBoards();
+        sleep.sleep(3);
         await downloadNoticeFiles.downloadHarvestedFiles();
+        sleep.sleep(3);
+        await broadcaster.postOnFacebook(credentials["facebook-id"], process.env.NOTICE_BOT_FB_PASSWORD || credentials["facebook-password"]);
     }
     catch (error) {
         console.log(error);
